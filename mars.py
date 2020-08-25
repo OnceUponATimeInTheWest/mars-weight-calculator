b8e4778467c110616859a234172d7ad5902a9b18
OnceUponATimeInTheWest <61394190+OnceUponATimeInTheWest@users.noreply.github.com>
2020-08-24 23:38:46 -0700

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,15 +1,9 @@
-weight = float(input('Enter your weight in pounds '))
+message = float(input('Enter your weight in pounds: '))
 
-#Gravity vars in m/s²
-mars_grav = 3.711
-earth_grav = 9.81
+weight = (message / float(9.81)) * 3.711
+approx = ("%.2f" % message)
 
-#Calculation
-weight2 = (weight / float(9.81)) * 3.711
-approxweight = ("%.2f" % weight2)
-
-#Result
-print('Your weight on Mars would be approximately ' +  str(approxweight) + ' lbs')
+print(f'Your weight on Mars would be approximately {approx} lbs')
 
 
 
